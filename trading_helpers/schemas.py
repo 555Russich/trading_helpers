20e17c5a60c2faf5f3ec055595b91cb6c7fcb1a3
Dmitriy Kutsokon <Kutsokondima@yandex.ru>
2024-05-16 20:30:47 +0300

    candle.time -> candle.dt

--- trading_helpers/schemas.py
+++ trading_helpers/schemas.py
@@ -15,7 +15,7 @@ class _Candle:
     low: float
     close: float
     volume: int
-    time: datetime
+    dt: datetime
 
     def multiple_by_constant(self, v: int | float) -> Self:
         return _Candle(
@@ -24,7 +24,7 @@ class _Candle:
             low=self.low*v,
             close=self.close*v,
             volume=self.volume,
-            time=self.time
+            dt=self.dt
         )
 
     def __add__(self, other: Self) -> Self:
@@ -34,7 +34,7 @@ class _Candle:
             low=self.low + other.low,
             close=self.close + other.close,
             volume=self.volume + other.volume,
-            time=self.time if self.time >= other.time else other.time
+            dt=self.dt if self.dt >= other.dt else other.dt
         )
 
     def __sub__(self, other: Self):
@@ -44,7 +44,7 @@ class _Candle:
             low=self.low - other.low,
             close=self.close - other.close,
             volume=self.volume + other.volume,
-            time=self.time if self.time >= other.time else other.time
+            dt=self.dt if self.dt >= other.dt else other.dt
         )
 
     def __mul__(self, other: Self):
@@ -54,7 +54,7 @@ class _Candle:
             low=self.low * other.low,
             close=self.close * other.close,
             volume=self.volume + other.volume,
-            time=self.time if self.time >= other.time else other.time
+            dt=self.dt if self.dt >= other.dt else other.dt
         )
 
     def __truediv__(self, other: Self):
@@ -64,7 +64,7 @@ class _Candle:
             low=self.low / other.low,
             close=self.close / other.close,
             volume=self.volume + other.volume,
-            time=self.time if self.time >= other.time else other.time
+            dt=self.dt if self.dt >= other.dt else other.dt
         )
 
 
@@ -73,9 +73,9 @@ class _Candles(UserList[_Candle]):
 
     def check_datetime_consistency(self) -> None:
         for i in range(1, len(self)):
-            if self[i-1].time > self[i].time:
+            if self[i-1].dt > self[i].dt:
                 raise IncorrectDatetimeConsistency(f'Previous candle datetime value later than previous candle has: '
-                                                   f'{self[i-1].time=} | {self[i].time=}')
+                                                   f'{self[i-1].dt=} | {self[i].dt=}')
 
     def remove_same_candles_in_a_row(self) -> Self:
         new_candles = _Candles()
@@ -83,7 +83,7 @@ class _Candles(UserList[_Candle]):
         for i in range(1, len(self)):
             c2 = self[i]
             if not (c1.open == c2.open and c1.high == c2.high and c1.low == c2.low and
-                    c1.close == c2.close and c1.volume == c2.volume and c1.time != c2.time):
+                    c1.close == c2.close and c1.volume == c2.volume and c1.dt != c2.dt):
                 new_candles.append(c1)
                 c1 = c2
 
@@ -91,14 +91,14 @@ class _Candles(UserList[_Candle]):
         return new_candles
 
     def remove_weekend_and_holidays_candles(self) -> Self:
-        candles = [c for c in self if c.time.weekday() not in (5, 6) and c.time.date() not in self.HOLIDAYS]
+        candles = [c for c in self if c.dt.weekday() not in (5, 6) and c.dt.date() not in self.HOLIDAYS]
         return self.__class__(candles)
 
     def remove_weekend_candles(self) -> Self:
-        return self.__class__([c for c in self if c.time.weekday() not in (5, 6)])
+        return self.__class__([c for c in self if c.dt.weekday() not in (5, 6)])
 
     def remove_holidays_candles(self) -> Self:
-        return self.__class__([c for c in self if c.time.date() not in self.HOLIDAYS])
+        return self.__class__([c for c in self if c.dt.date() not in self.HOLIDAYS])
 
     def _do_math_operation(self, func_name: MathOperation, other: Self) -> Self:
         if len(self) == 0 or len(other) == 0:
@@ -111,13 +111,13 @@ class _Candles(UserList[_Candle]):
             c1 = self[i1]
             c2 = other[i2]
 
-            if c1.time == c2.time:
+            if c1.dt == c2.dt:
                 i1 += 1
                 i2 += 1
-            elif c1.time > c2.time:
+            elif c1.dt > c2.dt:
                 c1 = self[i1 - 1]
                 i2 += 1
-            elif c1.time < c2.time:
+            elif c1.dt < c2.dt:
                 c2 = self[i2 - 1]
                 i1 += 1
 
